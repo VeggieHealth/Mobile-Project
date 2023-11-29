656b896f080995dfde36c2cd6ba21173c77fe30a
Kalistus Alvino <kalistus33@gmail.com>
2023-11-30 01:09:26 +0700

    Design Profile dan Memperbaiki Deisgn Home

--- app/src/main/java/com/example/veggiehealth/ui/person/PersonFragment.kt
+++ app/src/main/java/com/example/veggiehealth/ui/person/PersonFragment.kt
@@ -1,5 +1,6 @@
 package com.example.veggiehealth.ui.person
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.veggiehealth.R
 import com.example.veggiehealth.databinding.FragmentPersonBinding
+import com.example.veggiehealth.ui.login.LoginActivity
 
 
 class PersonFragment : Fragment() {
@@ -22,8 +24,13 @@ class PersonFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         _binding = FragmentPersonBinding.inflate(inflater, container, false)
+
+        binding.buttonLogoutProfile.setOnClickListener {
+            val logoutIntent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(logoutIntent)
+        }
+
         return binding.root
     }
     override fun onDestroyView() {
