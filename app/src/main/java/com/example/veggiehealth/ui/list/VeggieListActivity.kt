e3b3b6636a5dbdfe0cd706f3dd19056bf51dee22
Dion Defindra <sbwdion@gmail.com>
2023-12-04 16:56:42 +0700

    Membuat nav atas, List dan list Item

---
+++ app/src/main/java/com/example/veggiehealth/ui/list/VeggieListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.veggiehealth.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.veggiehealth.R
+
+class VeggieListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_veggie_list)
+    }
+}
