735a63d2e867a15465332b492a84a6f893df2101
Dion Defindra <sbwdion@gmail.com>
2023-12-10 02:39:07 +0700

    Membuat Quiz

---
+++ app/src/main/java/com/example/veggiehealth/ui/game/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.veggiehealth.ui.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.veggiehealth.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
