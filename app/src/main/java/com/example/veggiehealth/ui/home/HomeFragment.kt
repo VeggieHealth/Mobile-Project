b2252f240fb771028f5b8399a1d2a9699457d952
Kalistus Alvino <kalistus33@gmail.com>
2023-12-19 00:00:25 +0700

    Fix Main code

--- app/src/main/java/com/example/veggiehealth/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/veggiehealth/ui/home/HomeFragment.kt
@@ -1,30 +1,18 @@
 package com.example.veggiehealth.ui.home
 
 import android.content.Intent
-import android.graphics.Camera
-import android.content.Intent
 import android.os.Bundle
-import android.app.ListActivity
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.Fragment
-import com.example.veggiehealth.R
 import com.example.veggiehealth.databinding.FragmentHomeBinding
 import com.example.veggiehealth.ui.camera.CameraActivity
 import com.example.veggiehealth.ui.list.VeggieListActivity
 import com.example.veggiehealth.ui.game.GameActivity
-import com.google.android.material.card.MaterialCardView
-
-import com.example.veggiehealth.ui.list.VeggieListActivity
 
 
-class HomeFragment : Fragment(), View.OnClickListener {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -41,7 +29,6 @@ class HomeFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Menggunakan aktivitas yang mengandung fragment
         val parentActivity = activity
 
         binding.cvScan.setOnClickListener {
@@ -49,30 +36,12 @@ class HomeFragment : Fragment(), View.OnClickListener {
             startActivity(intent)
         }
         binding.btnListHome.setOnClickListener {
-            // Membuat intent dan memulai aktivitas dari aktivitas yang mengandung fragment
             val intent = Intent(parentActivity, VeggieListActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val cardViewQuiz: MaterialCardView = view.findViewById(R.id.cv_quiz)
-        cardViewQuiz.setOnClickListener {
-            // Start GameActivity when cv_quiz button is clicked
-            val intent = Intent(activity, GameActivity::class.java)
+        binding.cvQuiz.setOnClickListener {
+            val intent = Intent(parentActivity, GameActivity::class.java)
             startActivity(intent)
-        }
-
-    }
-
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.cv_scan -> {
-                // Handle click for cv_scan button
-                // Add the logic for what you want to do when cv_scan is clicked
-            }
         }
     }
 
