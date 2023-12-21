85e94219acc6a5e9cafc4eb14fed302e63d24021
Dion Defindra <sbwdion@gmail.com>
2023-12-21 12:42:54 +0700

    mengubah nama dihomefragment

--- app/src/main/java/com/example/veggiehealth/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/veggiehealth/ui/home/HomeFragment.kt
@@ -7,17 +7,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.example.veggiehealth.R
+import com.example.veggiehealth.ViewModelFactory
+import com.example.veggiehealth.data.remote.response.UserProfile
 import com.example.veggiehealth.databinding.FragmentHomeBinding
+import com.example.veggiehealth.di.ResultState
 import com.example.veggiehealth.ui.camera.CameraActivity
 import com.example.veggiehealth.ui.list.VeggieListActivity
 import com.example.veggiehealth.ui.game.GameActivity
+import com.example.veggiehealth.ui.person.ProfileViewModel
 
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val profileViewModel: ProfileViewModel by viewModels {
+        ViewModelFactory.getInstance(requireActivity().application)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,7 +55,20 @@ class HomeFragment : Fragment() {
             val intent = Intent(parentActivity, GameActivity::class.java)
             startActivity(intent)
         }
+        profileViewModel.getDetailProfile().observe(viewLifecycleOwner) { profileResult ->
+            when (profileResult) {
 
+                is ResultState.Success -> {
+                    val userData = profileResult.data.userProfile
+                    getDetailuser(userData)
+                }
+
+                is ResultState.Error -> {
+                    showToast(profileResult.error)
+                }
+                else -> Unit
+            }
+        }
     }
     private fun updateTimeOfDayGreeting() {
         val calendar = Calendar.getInstance()
@@ -55,7 +78,6 @@ class HomeFragment : Fragment() {
             in 6..11 -> "Good Morning"
             in 12..17 -> "Good Afternoon"
             in 18..23 -> "Good Evening"
-            in 23..23 -> "Good Evening"
             in 0..5 -> "Good Night"
             else -> "Hello"
         }
@@ -63,6 +85,15 @@ class HomeFragment : Fragment() {
 
         binding.tvWelcomeHome.text = greetingText
     }
+
+    private fun getDetailuser(user: UserProfile) {
+        binding.tvNamaHome.text = user.userProfile
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
